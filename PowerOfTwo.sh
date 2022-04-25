757f38425dcf427046e6935b3856b0b6578ac272
ChetanGavanali <chetangavanali@gmail.com>
2022-04-26 02:46:51 +0530

    Added all practice files

---
+++ PowerOfTwo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+read -p "Enter the number : " number
+read -p "Enter the power : " power
+power_of_number=1
+for (( i=1; i<=$power; i++ ))
+do
+power_of_number=$(( power_of_number * number ))
+done
+echo " $number ^ $power : $power_of_number"
---
+++ PrimeRange.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+echo "Enter a number upto which you want the prime numbers: "
+read n
+echo "Prime numbers upto $n are:"
+for (( i=2; i<=n; i++ ))
+do
+flag=0
+for (( j=2; j<i; j++ ))
+do
+if [ `expr $i % $j` -eq 0 ]
+then
+flag=1
+fi
+done
+if [ $flag -eq 0 ]
+then
+echo $i
+fi
+done
